1ef1d9855724c6ed05d39837dc8716e980bd49ec
HieuNM <nmhieucoder@gmail.com>
2022-10-13 21:29:55 +0700

    update test notion

--- hm_laravel_45_Cypress_test_auto/cypress/e2e/test notion.cy.js
+++ hm_laravel_45_Cypress_test_auto/cypress/e2e/test notion.cy.js
@@ -1,4 +1,4 @@
-describe('Test Flow Contect Sales Notion Tablet', () => {
+describe('Test Flow Cntact Sales Notion Tablet', () => {
   it('Open website', ()=> {
     cy.visit('https://www.notion.so/')
   })
@@ -13,22 +13,50 @@ describe('Test Flow Contect Sales Notion Tablet', () => {
 
   it('Test No Fill Value', ()=> {
     cy.get('.jsx-ec2682ece98d48f .submit-wrap .button-size-l').click()
-   
   })
   it('Check Email Error', ()=> {
-    // cy.get('.jsx-2655736279.jsx-3868764881.input').click()
-    cy.get('.jsx-2655736279.jsx-3868764881.input')
-    .invoke('attr', 'value', 'test123')
+    // Điền thông tin form hợp lệ
+    cy.get(':nth-child(2) > .input-wrap > .jsx-2655736279')
+    .type('test last name')
+    cy.get('.jsx-3153388610 > :nth-child(1) > .input-wrap > .jsx-2655736279')
+    .type('test first name')
+    cy.get(':nth-child(3) > label.jsx-3455048443 > .input-wrap > .jsx-2655736279')
+    .type('test Company ')
+    cy.get('.select-wrap select[name="companySize"]').select('1-19 employees') 
+    cy.get('.select-wrap select[name="country"]').select('Åland Islands') 
 
+    //Điền thông tin email sai
     cy.get('input[name="email"]')
-    .invoke('attr', 'value', 'hieunm47fpt.con.vn')
-    .should('have.attr', 'value', 'hieunm47fpt.con.vn')
+    .type('hieunm47fpt.com.vn')
 
-    // cy.get('.jsx-ec2682ece98d48f .submit-wrap .button-size-l').click()
+    cy.get('.jsx-ec2682ece98d48f .submit-wrap .button-size-l').click()
+  })
+  it('Check Email OK', ()=> {
 
+    // reset value
+    cy.get(':nth-child(2) > .input-wrap > .jsx-2655736279').clear()
+    cy.get('.jsx-3153388610 > :nth-child(1) > .input-wrap > .jsx-2655736279').clear()
+    cy.get(':nth-child(2) > label.jsx-3455048443 > .input-wrap > .jsx-2655736279').clear()
+    cy.get(':nth-child(3) > label.jsx-3455048443 > .input-wrap > .jsx-2655736279').clear()
+    cy.get('input[name="email"]').clear()
+    cy.get('.select-wrap select[name="companySize"]').select('1-19 employees') 
+    cy.get('.select-wrap select[name="country"]').select('Åland Islands') 
 
-    // cy.get('.select-wrap select[name="companySize"]').select('1-19 employees') 
-    // cy.get('.select-wrap select[name="country"]').select('Åland Islands') 
+    // Điền thông tin form hợp lệ
+    cy.get(':nth-child(2) > .input-wrap > .jsx-2655736279')
+    .type('test last name')
+    cy.get('.jsx-3153388610 > :nth-child(1) > .input-wrap > .jsx-2655736279')
+    .type('test first name')
+    cy.get(':nth-child(3) > label.jsx-3455048443 > .input-wrap > .jsx-2655736279')
+    .type('test Company ')
+    cy.get('.select-wrap select[name="companySize"]').select('1-19 employees') 
+    cy.get('.select-wrap select[name="country"]').select('Åland Islands') 
+
+    //Điền thông tin email sai
+    cy.get('input[name="email"]')
+    .type('hieunm47@fpt.com.vn')
+    
+    cy.get('.jsx-448f407d1ced1fc8 > .jsx-2971364727').click()
   })
 
 })
