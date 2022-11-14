dba902e367d846386a83633dc7b7017b94cbfc39
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-11-14 14:44:29 +0700

    hm_laravel_48_laravel_file_manager

---
+++ hm_laravel_48_laravel_file_manager/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// https://www.itsolutionstuff.com/post/laravel-file-manager-tutorial-step-by-stepexample.html
+// Link tutorial 
