86a1aa85f53121f2ad51bae47ed242e34f8d2b4d
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-08-07 17:31:23 +0700

    hm_laravel_56_OAuth2 - push authen - còn author

--- hm_laravel_56_OAuth2/routes/web.php
+++ hm_laravel_56_OAuth2/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//https://blog.logrocket.com/laravel-passport-a-tutorial-and-example-build/
+
+// ------------------- composer require laravel/passport -W
+// Trong Laravel, tùy chọn -W khi chạy lệnh composer là viết tắt của --update-with-dependencies. Khi bạn thêm tùy chọn này vào lệnh composer require,
+// nó sẽ cập nhật tất cả các gói phụ thuộc của gói bạn đang cài đặt lên phiên bản mới nhất, và sau đó cài đặt gói bạn yêu cầu.
