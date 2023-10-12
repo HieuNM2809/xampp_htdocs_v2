8be4dca8237beeb860d572fe5367374a2a966885
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-10-12 15:55:37 +0700

    hm_laravel_59_laravel_telescope - push base

--- hm_laravel_59_laravel_telescope/routes/web.php
+++ hm_laravel_59_laravel_telescope/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//https://www.itsolutionstuff.com/post/laravel-telescope-installation-and-configuration-tutorialexample.html
